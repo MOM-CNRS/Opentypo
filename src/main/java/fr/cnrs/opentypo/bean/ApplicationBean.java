8de1ede2bc873e83e81bed357ad3add2daecb007
GABSI <firas.gabsi@gmail.com>
2025-12-26 10:56:31 +0100

    Ajout de la liste des catégories disponible dans la page référentiel

--- src/main/java/fr/cnrs/opentypo/bean/ApplicationBean.java
+++ src/main/java/fr/cnrs/opentypo/bean/ApplicationBean.java
@@ -3,13 +3,9 @@ package fr.cnrs.opentypo.bean;
 import fr.cnrs.opentypo.models.Language;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.SessionScoped;
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
-import org.primefaces.PrimeFaces;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -22,12 +18,22 @@ import java.util.List;
 public class ApplicationBean implements Serializable {
 
     public List<Language> languages;
+    
+    private boolean showReferentielPanel = false;
 
     @PostConstruct
     public void initialization() {
         languages = new ArrayList<Language>();
         languages.add(new Language(1, "fr", "Français", "fr"));
         languages.add(new Language(2, "an", "Anglais", "an"));
+    }
+    
+    public void showReferentiel() {
+        showReferentielPanel = true;
+    }
+    
+    public boolean isShowCards() {
+        return !showReferentielPanel;
     }
 }
 
