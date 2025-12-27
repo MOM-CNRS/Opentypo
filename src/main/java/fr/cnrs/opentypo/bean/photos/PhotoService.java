e9309020d648bc085f0ee10dce01447c1f639a75
GABSI <firas.gabsi@gmail.com>
2025-12-27 02:03:42 +0100

    Ajout du composant graphicImage dans la page type

---
+++ src/main/java/fr/cnrs/opentypo/bean/photos/PhotoService.java
@@ -0,0 +1,38 @@
+package fr.cnrs.opentypo.bean.photos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+
+@Named
+@ApplicationScoped
+public class PhotoService {
+
+    private List<Photo> photos;
+
+    @PostConstruct
+    public void init() {
+        photos = new ArrayList<>();
+
+        photos.add(new Photo("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "Description for Image 1", "Title 1"));
+        photos.add(new Photo("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "Description for Image 2", "Title 2"));
+        photos.add(new Photo("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "Description for Image 3", "Title 3"));
+        photos.add(new Photo("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/474000/474240-Left-Bank-Paris.jpg",
+                "Description for Image 4", "Title 4"));
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+}
