6aabcc2edc0c25e98a489bf9b37d30e5eb8b2770
GABSI <firas.gabsi@gmail.com>
2025-12-29 14:00:35 +0100

    Ajout de la configuration des logs + deux profiles spring

--- src/main/java/fr/cnrs/opentypo/entity/ReferentielOpentheso.java
+++ src/main/java/fr/cnrs/opentypo/entity/ReferentielOpentheso.java
@@ -28,7 +28,7 @@ public class ReferentielOpentheso implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "code", nullable = false, length = 500)
+    @Column(name = "code", nullable = false)
     private String code;
 
     @Column(name = "valeur", nullable = false, length = 500)
